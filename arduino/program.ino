26dcdc6c18dc73f8d9e824eb31b289116bd3a39b
FayVince <118724170+FayVince@users.noreply.github.com>
2026-01-04 20:33:20 +0100

    Refactor BLE resistance meter to use NimBLE API

--- arduino/program.ino
+++ arduino/program.ino
@@ -1,147 +1,123 @@
 /*
- * ESP32-C3 BLE Ellenállás Mérő
- * Kompatibilis a FayVince/alkalmazas Android alkalmazással
- * 
- * BLE Service UUID: 0000ffe0-0000-1000-8000-00805f9b34fb
- * BLE Characteristic UUID: 0000ffe1-0000-1000-8000-00805f9b34fb
- * 
- * Adat formátum: 2 byte (Low byte first, High byte second)
- * Küldési frekvencia: 1 másodpercenként
+ * ESP32-C3 BLE Ellenállás Mérő (NimBLE - API kompatibilis, egyszerű)
+ *
+ * Service UUID:        0000ffe0-0000-1000-8000-00805f9b34fb
+ * Characteristic UUID: 0000ffe1-0000-1000-8000-00805f9b34fb
+ *
+ * 2 byte little-endian notify, 1 mp-enként
  */
 
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include <Arduino.h>
+#include <NimBLEDevice.h>
 
-// BLE UUID-k - megegyeznek az Android app-ban használtakkal
-#define SERVICE_UUID        "0000ffe0-0000-1000-8000-00805f9b34fb"
-#define CHARACTERISTIC_UUID "0000ffe1-0000-1000-8000-00805f9b34fb"
+static const char* DEVICE_NAME         = "BLE_Resistance_Meter";
+static const char* SERVICE_UUID        = "0000ffe0-0000-1000-8000-00805f9b34fb";
+static const char* CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb";
 
-// Analóg bemenet pin az ellenállás méréshez
-#define ANALOG_PIN 0  // GPIO0 az ESP32-C3-n (ADC1_CH0)
+#define ANALOG_PIN 0   // ESP32-C3 GPIO0 (ADC1_CH0)
 
-// BLE változók
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
+static NimBLEServer* pServer = nullptr;
+static NimBLECharacteristic* pCharacteristic = nullptr;
 
-// Időzítés
-unsigned long previousMillis = 0;
-const long interval = 1000;  // 1 másodperces intervallum
+static bool deviceConnected = false;
+static unsigned long lastSend = 0;
+static const unsigned long SEND_INTERVAL_MS = 1000;
 
-// Callback osztály a kapcsolat állapotának kezelésére
-class MyServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        deviceConnected = true;
-        Serial.println("Eszköz csatlakozva!");
-    }
+class MyServerCallbacks : public NimBLEServerCallbacks {
+  void onConnect(NimBLEServer* server, NimBLEConnInfo& info) override {
+    deviceConnected = true;
+    Serial.print("Csatlakozott: ");
+    Serial.println(info.getAddress().toString().c_str());
+  }
 
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
-        Serial.println("Eszköz lecsatlakozva!");
-    }
+  void onDisconnect(NimBLEServer* server, NimBLEConnInfo& info, int reason) override {
+    deviceConnected = false;
+    Serial.print("Lecsatlakozott, reason=");
+    Serial.println(reason);
+
+    delay(150);
+    NimBLEDevice::startAdvertising();
+    Serial.println("Advertising újraindítva");
+  }
 };
 
+uint16_t readResistance() {
+  int adc = analogRead(ANALOG_PIN);
+  if (adc < 0) adc = 0;
+  if (adc > 4095) adc = 4095;
+  return (uint16_t)adc;
+}
+
+void startAdvertising() {
+  NimBLEAdvertising* adv = NimBLEDevice::getAdvertising();
+  adv->stop();
+
+  NimBLEAdvertisementData ad;
+  ad.setFlags(0x06); // LE General Discoverable + BR/EDR not supported
+  ad.setCompleteServices(NimBLEUUID(SERVICE_UUID));
+
+  NimBLEAdvertisementData sr;
+  sr.setName(DEVICE_NAME);
+
+  adv->setAdvertisementData(ad);
+  adv->setScanResponseData(sr);
+
+  adv->start();
+  Serial.println("Advertising elindítva");
+}
+
+void startBle() {
+  NimBLEDevice::init(DEVICE_NAME);
+
+  pServer = NimBLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  NimBLEService* service = pServer->createService(SERVICE_UUID);
+
+  // READ + WRITE + NOTIFY (app kompatibilisebb)
+  pCharacteristic = service->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY
+  );
+
+  uint8_t initData[2] = {0x00, 0x00};
+  pCharacteristic->setValue(initData, 2);
+
+  service->start();
+
+  startAdvertising();
+}
+
 void setup() {
-    Serial.begin(115200);
-    Serial.println("ESP32-C3 BLE Ellenállás Mérő indítása...");
+  Serial.begin(115200);
+  delay(200);
 
-    // Analóg bemenet beállítása
-    analogReadResolution(12);  // 12-bit felbontás (0-4095)
-    analogSetAttenuation(ADC_11db);  // 0-3. 3V tartomány
+  Serial.println("ESP32-C3 BLE Ellenállás Mérő (NimBLE)");
 
-    // BLE inicializálás
-    BLEDevice::init("BLE_Resistance_Meter");
+  analogReadResolution(12);
+  analogSetAttenuation(ADC_11db);
 
-    // BLE szerver létrehozása
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-
-    // BLE szolgáltatás létrehozása
-    BLEService* pService = pServer->createService(SERVICE_UUID);
-
-    // BLE karakterisztika létrehozása NOTIFY tulajdonsággal
-    pCharacteristic = pService->createCharacteristic(
-        CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY
-    );
-
-    // Descriptor hozzáadása a NOTIFY-hoz (CCCD - Client Characteristic Configuration Descriptor)
-    pCharacteristic->addDescriptor(new BLE2902());
-
-    // Szolgáltatás indítása
-    pService->start();
-
-    // Hirdetés indítása
-    BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06);
-    pAdvertising->setMinPreferred(0x12);
-    BLEDevice::startAdvertising();
-
-    Serial.println("BLE szerver elindítva, várakozás kapcsolatra...");
-    Serial.println("Eszköz neve: BLE_Resistance_Meter");
+  startBle();
 }
 
 void loop() {
-    unsigned long currentMillis = millis();
+  unsigned long now = millis();
 
-    // Másodpercenként küld adatot ha van csatlakozott eszköz
-    if (currentMillis - previousMillis >= interval) {
-        previousMillis = currentMillis;
+  if (deviceConnected && (now - lastSend >= SEND_INTERVAL_MS)) {
+    lastSend = now;
 
-        if (deviceConnected) {
-            // Ellenállás érték olvasása (analóg bemenetről)
-            uint16_t resistanceValue = readResistance();
+    uint16_t value = readResistance();
 
-            // 2 byte-os adat előkészítése (Low byte first - Little Endian)
-            uint8_t data[2];
-            data[0] = resistanceValue & 0xFF;         // Low byte
-            data[1] = (resistanceValue >> 8) & 0xFF;  // High byte
+    uint8_t data[2];
+    data[0] = (uint8_t)(value & 0xFF);
+    data[1] = (uint8_t)((value >> 8) & 0xFF);
 
-            // Adat küldése BLE-n keresztül
-            pCharacteristic->setValue(data, 2);
-            pCharacteristic->notify();
+    pCharacteristic->setValue(data, 2);
+    pCharacteristic->notify();
 
-            Serial.print("Küldött érték: ");
-            Serial.print(resistanceValue);
-            Serial.print(" (0x");
-            Serial.print(data[1], HEX);
-            Serial.print(data[0], HEX);
-            Serial.println(")");
-        }
-    }
+    Serial.print("Küldve: ");
+    Serial.println(value);
+  }
 
-    // Újracsatlakozás kezelése
-    if (! deviceConnected && oldDeviceConnected) {
-        delay(500);  // Várakozás a Bluetooth stack rendezésére
-        pServer->startAdvertising();
-        Serial.println("Hirdetés újraindítva...");
-        oldDeviceConnected = deviceConnected;
-    }
-
-    if (deviceConnected && !oldDeviceConnected) {
-        oldDeviceConnected = deviceConnected;
-    }
-}
-
-// Ellenállás érték olvasása
-// Módosítsd ezt a függvényt a konkrét szenzor/áramkör szerint! 
-uint16_t readResistance() {
-    // Analóg érték olvasása (0-4095 12-bit)
-    int analogValue = analogRead(ANALOG_PIN);
-    
-    // Példa: direkt ADC érték használata
-    // Ha feszültségosztóval mérsz ellenállást, itt számold át
-    // Például: R_mérő = R_ismert * (Vcc/Vmért - 1)
-    
-    // Az Android app 500-2000 közötti értékeket vár a demo módban
-    // Skálázd az értéket a szükséges tartományra
-    uint16_t resistanceValue = map(analogValue, 0, 4095, 0, 4095);
-    
-    return resistanceValue;
+  delay(10);
 }
